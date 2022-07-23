f8b355af2c50de4fa07abbfe03230f158b29a42e
mokashi21 <101655816+mokashi21@users.noreply.github.com>
2022-07-23 20:18:22 +0530

    Add files via upload

---
+++ Astoguru.js
@@ -0,0 +1,21 @@
+// let sayHii = function() {
+//     console.log(" Hii how are ypu");
+// };
+
+// sayHii();
+// 
+
+function Isastoguru (job,location,wname,kids) {
+    console.log(job + "" + location + "" + wname + "" + kids);
+   
+}
+
+let job = "you are in a software-Engineer";
+let location = " at russia";
+let wname = " and married to irina";
+let kids = " - with 3 kids";
+
+Isastoguru(job,location,wname,kids);
+
+
+
